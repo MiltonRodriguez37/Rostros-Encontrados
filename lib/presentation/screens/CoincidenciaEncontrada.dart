8098692852c3db97d9b00f5dd00c8eb3a196e710
MiltonRodriguez37 <milton.rodriguezg37@gmail.com>
2024-04-23 02:08:13 -0600

    Envio de json id y porcentaje

--- lib/presentation/screens/CoincidenciaEncontrada.dart
+++ lib/presentation/screens/CoincidenciaEncontrada.dart
@@ -1,111 +1,119 @@
 import 'package:flutter/material.dart';
+class CoincidenciaEncontrada extends StatefulWidget {
+  final String idCoincidencia;
+  final double porcentajeCoincidencia;
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-    @override
-  Widget build(BuildContext context) {
-    return const Home();
-  }
-}
-
-
-class Home extends StatefulWidget {
-  const Home({super.key});
+  const CoincidenciaEncontrada({
+    required this.idCoincidencia,
+    required this.porcentajeCoincidencia,
+  });
 
   @override
-  State<Home> createState() => _HomeState();
+  State<CoincidenciaEncontrada> createState() => _CoincidenciaEncontradaState();
 }
 
-class _HomeState extends State<Home> {
+class _CoincidenciaEncontradaState extends State<CoincidenciaEncontrada> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: cuerpo(),
+    );
+  }
 
-      body: cuerpo()
-      
+  Widget cuerpo() {
+    return Container(
+      color: Color.fromARGB(255, 253, 229, 8),
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 50),
+          mensaje1(),
+          SizedBox(height: 25),
+          mensaje2(),
+          SizedBox(height: 50),
+          imagenes(),
+          SizedBox(height: 15),
+          widgetsAbajo(),
+          SizedBox(height: 20),
+          Text(
+            'ID de Coincidencia: ${widget.idCoincidencia}',
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          Text(
+            'Porcentaje de Coincidencia: ${widget.porcentajeCoincidencia.toStringAsFixed(2)}%',
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget mensaje1() {
+    return Text(
+      "¡Coincidencia encontrada!",
+      textAlign: TextAlign.center,
+      style: TextStyle(color: Colors.black, fontSize: 20.0),
+    );
+  }
+
+  Widget mensaje2() {
+    return Text(
+      "La persona a cargo del registro ha sido notificada vía correo electrónico y teléfono.",
+      textAlign: TextAlign.center,
+      style: TextStyle(color: Colors.black, fontSize: 18.0),
+    );
+  }
+
+  Widget imagenes() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.network(
+              'https://thumbs.dreamstime.com/b/portrait-innocence-innocent-kid-slum-islamabad-pakistan-67584463.jpg',
+              fit: BoxFit.cover,
+              width: 125.0,
+              height: 175.0,
+            ),
+            SizedBox(height: 15),
+            Text(
+              'Foto tomada por ti',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.black, fontSize: 17.0),
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.network(
+              'https://thumbs.dreamstime.com/b/portrait-innocence-innocent-kid-slum-islamabad-pakistan-67584463.jpg',
+              fit: BoxFit.cover,
+              width: 125.0,
+              height: 175.0,
+            ),
+            SizedBox(height: 15),
+            Text(
+              'Foto registrada',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.black, fontSize: 17.0),
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget widgetsAbajo() {
+    return Text(
+      "Widgets abajo.",
+      textAlign: TextAlign.center,
+      style: TextStyle(color: Colors.black, fontSize: 15.0),
     );
   }
 }
-
-Widget cuerpo(){
-  return Container (
-    color: Color.fromARGB(255, 253, 229, 8),
-    child: Column (
-        children: <Widget>[
-        SizedBox(height: 50,),
-        mensaje1(),
-        SizedBox(height: 25,),
-        mensaje2(),
-        SizedBox(height: 50,),
-        imagenes(),
-        SizedBox(height: 15,),
-        widgetsAbajo(),
-      ],
-    ),
-  );
-}
-
-
-Widget mensaje1(){
-  return Text("¡Coincidencia encontrada!", textAlign: TextAlign.center, style: TextStyle(color: Colors.black, fontSize: 20.0),);
-}
-
-Widget mensaje2(){
-  return Text("La persona a cargo del registro ha sido notificada via correo electrónico y teléfono.", textAlign: TextAlign.center, style: TextStyle(color: Colors.black, fontSize: 18.0),);
-}
-
-Widget imagenes(){
-
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    children: <Widget>[
-      Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Image.network(
-            'https://thumbs.dreamstime.com/b/portrait-innocence-innocent-kid-slum-islamabad-pakistan-67584463.jpg',
-            fit: BoxFit.cover,
-            width: 125.0,  // Ancho deseado
-            height: 175.0, // Altura deseada
-          ),
-          SizedBox(height: 15),
-          Text(
-            'Foto tomada por ti',
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.black, fontSize: 17.0),
-          ),
-          SizedBox(height: 20),
-        ],
-      ),
-      Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Image.network(
-            'https://thumbs.dreamstime.com/b/portrait-innocence-innocent-kid-slum-islamabad-pakistan-67584463.jpg',
-            fit: BoxFit.cover,
-            width: 125.0,  // Ancho deseado
-            height: 175.0, // Altura deseada
-          ),
-          SizedBox(height: 15),
-          Text(
-            'Foto registrada',
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.black, fontSize: 17.0),
-          ),
-          SizedBox(height: 20),
-        ],
-      ),
-    ],
-  );
-}
-
-Widget widgetsAbajo(){
-   return Text("Widgets abajo.", textAlign: TextAlign.center, style: TextStyle(color: Colors.black, fontSize: 15.0),);
-}
-//ctrl + espacio, ver opciones
-
--- lib/presentation/screens/camara.dart
+++ lib/presentation/screens/camara.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rostros_encontrados/presentation/screens/CoincidenciaEncontrada.dart';
 import 'package:rostros_encontrados/presentation/screens/user.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:image_picker/image_picker.dart';
@@ -8,104 +9,157 @@ import 'package:rostros_encontrados/shared/services/upload_picture_firebase_enco
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-void _descargarImagen(nombreImagen) async {
-    final url2 = Uri.parse('http://rostrosencontrados.pythonanywhere.com/comparar_imagenes');
-    final body = jsonEncode({
-      'nombre': nombreImagen,
-    });
+class Camara extends StatefulWidget {
+  final User? usuario;
 
-    // Realiza la solicitud HTTP POST
-    final responsePost = await http.post(
-      url2,
-      headers: {'Content-Type': 'application/json'},
-      body: body,
-    );
+  const Camara({Key? key, this.usuario}) : super(key: key);
 
-    // Verifica el estado de la respuesta
-    if (responsePost.statusCode == 200) {
-      // La solicitud fue exitosa
-      print('Datos enviados exitosamente');
-    } else {
-      // Hubo un error en la solicitud
-      print('Error al descargar imagen ${responsePost.statusCode}');
-    }
+  @override
+  _CamaraState createState() => _CamaraState();
 }
 
-Storage storage = Storage();
+class _CamaraState extends State<Camara> {
+  bool isLoading = false;
+   final Storage storage = Storage();
 
-class Camara extends StatelessWidget {
-    final User? usuario;
-  const Camara({super.key,this.usuario});
+void _mostrarMensajeError(BuildContext context,error) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('¡Error!'),
+        content: Text(error),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('Aceptar'),
+          ),
+        ],
+      );
+    },
+  );
+}
+
+  void _descargarImagen(BuildContext context, String nombreImagen) async {
+    setState(() {
+      isLoading = true; // Activar indicador de carga
+    });
+
+    try {
+      final url2 = Uri.parse('http://rostrosencontrados.pythonanywhere.com/comparar_imagenes');
+      final body = jsonEncode({
+        'nombre': nombreImagen,
+      });
+
+      // Realiza la solicitud HTTP POST
+      final responsePost = await http.post(
+        url2,
+        headers: {'Content-Type': 'application/json'},
+        body: body,
+      );
+
+      if (responsePost.statusCode == 200) {
+        // Realiza la solicitud HTTP GET
+        final responseGet = await http.get(url2);
+
+        if (responseGet.statusCode == 200) {
+          // La solicitud fue exitosa, procesa la respuesta
+          final data = json.decode(responseGet.body);
+          String? idCoincidencia = data['idCoincidencia'];
+          double? porcentajeCoincidencia = data['porcentajeCoincidencia'];
+
+          // Navega a la nueva pantalla con los datos recibidos
+          if (idCoincidencia != null && porcentajeCoincidencia != null) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => CoincidenciaEncontrada(
+                  idCoincidencia: idCoincidencia,
+                  porcentajeCoincidencia: porcentajeCoincidencia,
+                ),
+              ),
+            );
+          } else {
+            print('Error: Datos de coincidencia incompletos');
+          }
+        } else {
+          print('Error en la respuesta GET ${responseGet.statusCode}');
+          print('Respuesta JSON: ${responseGet.body}');
+        }
+      } else {
+        //añadir mensaje en pantalla
+        print('Error al descargar imagenjhdjd ${responsePost.statusCode}');
+        _mostrarMensajeError(context,'Error al cargar la imagen, intente de nuevo');
+      }
+    } catch (e) {
+      print('Error en la solicitud HTTP: $e');
+    }
+
+    setState(() {
+      isLoading = false; // Desactivar indicador de carga
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Container(
         height: 500.0,
         width: MediaQuery.of(context).size.width,
-        decoration: const BoxDecoration(color: Color.fromARGB(255, 255, 255, 255), borderRadius: BorderRadius.all(Radius.circular(15))),
-        padding: const EdgeInsets.all(20.0),
-        margin: const EdgeInsets.symmetric(
-          horizontal: 10,
-          vertical: 10,
+        decoration: BoxDecoration(
+          color: Color.fromARGB(255, 255, 255, 255),
+          borderRadius: BorderRadius.all(Radius.circular(15)),
         ),
+        padding: const EdgeInsets.all(20.0),
+        margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
+            Text(
               "Selecciona una opción:",
               style: TextStyle(
                 fontSize: 28.0,
                 color: Color.fromARGB(255, 0, 0, 0),
               ),
             ),
-            const SizedBox(
-              height: 40,
-            ),
+            SizedBox(height: 40),
             Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                TextButton.icon(
-                  icon: const Icon(Icons.image, color: Color.fromARGB(255, 0, 0, 0),),
+                ElevatedButton.icon(
+                  icon: Icon(Icons.image, color: Color.fromARGB(255, 0, 0, 0)),
                   onPressed: () async {
                     final results = await FilePicker.platform.pickFiles();
 
-                  if (results == null) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Imagen no seleccionada'),
-                      ),
-                    );
-                    return null;
-                  }
+                    if (results == null) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Imagen no seleccionada'),
+                        ),
+                      );
+                      return;
+                    }
 
-                  else {
                     try {
                       final url = Uri.parse('http://rostrosencontrados.pythonanywhere.com/enviar_nombre_imagen');
-                      // Construye el cuerpo de la solicitud con los datos que deseas enviar
-
-                      // Realiza la solicitud HTTP POST
                       final response = await http.get(url);
 
                       var nombreImagen = '';
 
-                      // Verifica el estado de la respuesta
                       if (response.statusCode == 200) {
-                        // La solicitud fue exitosa
                         final data = json.decode(response.body);
                         nombreImagen = data['ultima_imagen'];
-                        print('Nombre de la imagen: $nombreImagen');
                         final ruta = results.files.single.bytes;
-                        print('Hasta aquí todo bien');
-                        await storage.subirArchivo(ruta, nombreImagen).then((value) => print('Imagen subida'));
-                        _descargarImagen(nombreImagen);
+                        await storage.subirArchivo(ruta, nombreImagen);
+                        _descargarImagen(context, nombreImagen);
                       } else {
-                        // Hubo un error en la solicitud
                         print('Error al enviar datos: ${response.statusCode}');
                       }
-                    }
-                    catch (e) {
+                    } catch (e) {
                       print('No se completó la acción deseada');
                     }
-                  } 
                   },
                   label: const Text("    Galería       ", style: TextStyle(color: Color.fromARGB(255, 0, 0, 0), fontSize: 22)),
                   style: TextButton.styleFrom(
@@ -114,56 +168,46 @@ class Camara extends StatelessWidget {
                     side: const BorderSide(width: 1, color: Colors.black)
                   ),
                 ),
-                const SizedBox(height: 20,),
-                TextButton.icon(
-                  icon: const Icon(Icons.camera, color: Color.fromARGB(255, 0, 0, 0),),
+                SizedBox(height: 20),
+                ElevatedButton.icon(
+                  icon: Icon(Icons.camera, color: Color.fromARGB(255, 0, 0, 0)),
                   onPressed: () async {
                     final picker = ImagePicker();
-                  final results = await picker.pickImage(
-                                          source: ImageSource.camera,
-                                          imageQuality: 80,
-                                          preferredCameraDevice: CameraDevice.rear
-                                          );
-
-                  if (results == null) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Imagen no seleccionada'),
-                      ),
+                    final results = await picker.pickImage(
+                      source: ImageSource.camera,
+                      imageQuality: 80,
+                      preferredCameraDevice: CameraDevice.rear,
                     );
-                    return null;
-                  }
 
-                  else {
+                    if (results == null) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Imagen no seleccionada'),
+                        ),
+                      );
+                      return;
+                    }
+
                     try {
                       final url = Uri.parse('http://rostrosencontrados.pythonanywhere.com/enviar_nombre_imagen');
-                      // Construye el cuerpo de la solicitud con los datos que deseas enviar
-
-                      // Realiza la solicitud HTTP POST
                       final response = await http.get(url);
 
                       var nombreImagen = '';
 
-                      // Verifica el estado de la respuesta
                       if (response.statusCode == 200) {
-                        // La solicitud fue exitosa
                         final data = json.decode(response.body);
                         nombreImagen = data['ultima_imagen'];
-                        print('Nombre de la imagen: $nombreImagen');
                         List<int> fileBytes = await results.readAsBytes();
                         Uint8List uint8List = Uint8List.fromList(fileBytes);
                         final ruta = uint8List;
-                        print('Hasta aquí todo bien');
-                        await storage.subirArchivo(ruta, nombreImagen).then((value) => print('Imagen subida'));
-                        _descargarImagen(nombreImagen);
+                        await storage.subirArchivo(ruta, nombreImagen);
+                        _descargarImagen(context, nombreImagen);
                       } else {
-                        // Hubo un error en la solicitud
                         print('Error al enviar datos: ${response.statusCode}');
                       }
-                    }
-                    catch (e) {
+                    } catch (e) {
                       print('No se completó la acción deseada');
                     }
-                  }
                   },
                   label: const Text("     Cámara       ", style: TextStyle(color: Color.fromARGB(255, 0, 0, 0), fontSize: 22)),
                   style: TextButton.styleFrom(
@@ -172,8 +216,20 @@ class Camara extends StatelessWidget {
                     side: const BorderSide(width: 1, color: Colors.black)
                   ),
                 ),
+                const SizedBox(height: 20),
+                if (isLoading)
+                  const Column(
+                    children: [
+                      CircularProgressIndicator(),
+                      SizedBox(height: 20),
+                      Text(
+                        'Realizando análisis...',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
               ],
-            )
+            ),
           ],
         ),
       ),
